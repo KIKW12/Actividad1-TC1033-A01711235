7762001b3c8e1e81c44dfeb861518a553565e5a9
KIKW12 <142268928+KIKW12@users.noreply.github.com>
2023-10-31 10:53:38 -0600

    Add files via upload

---
+++ programa1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+
+float vCubo{0.0f};
+float lado{0.0f};
+cout << "Ingresa el volumen del cubo: ";
+cin >> vCubo;
+
+lado = pow(vCubo,1/3.0f);
+
+cout << "El lado del cubo de volumen " << vCubo <<"cm^3" << " es " << lado << "cm";
+
+    return 0;
+}
